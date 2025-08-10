5c36d16bcce94bf29609dc6b7266e0f2c770bc13
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-10 10:25:30 -0500

    Update Footer.js

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -8,7 +8,7 @@ export const Footer = (props) => {
           <li><a href="especialidades">ESPECIALIDADES</a></li>
           <li><a href="/ingreso">INGRESA TUS DATOS</a></li>
           <li><a href="/imagenes">IMÁGENES</a></li>
-          <li><a href="https://www.facebook.com/clinicaveterinariasantaprisca">SIGUENOS EN FACEBOOK</a></li>
+          <li><a href="https://www.facebook.com/clinicaveterinariaambato">SIGUENOS EN FACEBOOK</a></li>
         </ul>
         <p>&copy; {props.year} Clínica Veterinaria Proyecto Jorge Patricio Santamaría Cherrez</p>
       </footer>
